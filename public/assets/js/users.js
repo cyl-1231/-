b223fe1509a4a50d91b4333b95aababf5b26c468
cyl <cyl@qq.cn>
2019-10-22 11:28:00 +0800

    针对用户添加功能的修改第一次

---
+++ public/assets/js/users.js
@@ -0,0 +1,33 @@
+$('#userForm').on('submit',function(){
+    // alert(123)
+    // console.log($(this).serialize());
+    var formdata=$(this).serialize();
+    $.ajax({
+        type:'post',
+        url:'/users',
+        data:formdata,
+        success:function(){
+            location.reload();
+        },
+        error:function(){
+            alert('用户添加失败');
+        }
+    })
+    return false;
+})
+$('#avatar').on('change',function(){
+    console.log($(this)[0].files[0]);
+    var formData=new FormData();
+    formData.append('avatar',$(this)[0].files[0])
+    $.ajax({
+        type: "post",
+        url: "/upload",
+        data: formData,
+        processData:false,
+        contentType:false,
+        success: function (response) {
+            $('#preview').attr('src',response[0].avatar);
+            $('#hiddenAvatar').val(response[0].avatar)
+        }
+    });
+})
