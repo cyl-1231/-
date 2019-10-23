ff6737e6934099811d7865e5687dd12de9d74491
cyl <cyl@qq.cn>
2019-10-23 20:46:07 +0800

    分类修改与删除第一次提交

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -19,3 +19,26 @@ $.ajax({
         $('#addcate').html(html)
     }
 })
+$('#addcate').on('click','.edit',function(){
+    var id=$(this).attr('data-id');
+    $.ajax({
+        type:'get',
+        url:'/categories/'+id,
+        success:function(response){
+            console.log(response);
+            var html=template('editcateTpl',response);
+            $('#editCategory').html(html);
+        }
+    })
+})
+$('#addcate').on('click','.delete',function(){
+    // alert(1)
+    var id=$(this).attr('data-id');
+    $.ajax({
+        type:'delete',
+        url:'/categories/'+id,
+        success:function(response){
+           location.reload();
+        }
+    })
+})
